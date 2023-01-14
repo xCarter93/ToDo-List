f60531801eaefc6a162a89bb690228eaacd59c46
Patrick Carter <patrickcarter@Patricks-Laptop.local>
2023-01-14 15:51:39 -0500

    Got basic functionality of to do list working, i.e. dynamically adding li elements to the page

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+let items = [];
+
 
 app.get('/', (req, res) => {
 
@@ -16,9 +18,17 @@ app.get('/', (req, res) => {
     };
     var currentDay = today.toLocaleString('en-us', options);
 
-    res.render('list', {dayName: currentDay});
+    res.render('list', {dayName: currentDay, newListItems: items});
 
-})
+});
+
+app.post('/', (req, res) => {
+
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running`);
