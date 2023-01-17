142ce518c83b37dd0363b279ed78293f0f6f1499
Patrick Carter <patrickcarter@Patricks-Laptop.local>
2023-01-17 16:33:34 -0500

    Adding in Mongoose to handled Mongo DB changes

--- app.js
+++ app.js
@@ -2,17 +2,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(`${__dirname}/date.js`);
 
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const db = "todolistDB";
+const mongoURL = `mongodb+srv://xcarter93:ISXi7JvzRagD6Ub3@cluster0.jwhx2lt.mongodb.net/${db}?retryWrites=true&w=majority`;
+mongoose.connect(mongoURL);
+
+const itemsSchema = new mongoose.Schema({
+	name: String,
+});
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+	name: "Welcome to your ToDo List!",
+});
+const item2 = new Item({
+	name: "Hit the + button to add a new item.",
+});
+const item3 = new Item({
+	name: "<--- Hit this to delete an item.",
+});
+const defaultItems = [item1, item2, item3];
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-let items = [];
-let workItems = [];
-
 app.get("/", (req, res) => {
 	let day = date();
-	res.render("list", { listTitle: day, newListItems: items });
+	Item.find({}, (err, items) => {
+		if (items.length === 0) {
+			Item.insertMany(defaultItems, (err) => {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Successfully added items to database.");
+				}
+			});
+			res.redirect("/");
+		} else {
+			res.render("list", { listTitle: day, newListItems: items });
+		}
+	});
 });
 
 app.post("/", (req, res) => {
