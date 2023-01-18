bd4b54e290d8b9d354ade0da6d762c6bcae20b41
Patrick Carter <patrickcarter@Patricks-Laptop.local>
2023-01-18 10:59:18 -0500

    Trying to fix app.js

--- app.js
+++ app.js
@@ -120,6 +120,11 @@ app.post("/delete", (req, res) => {
 	}
 });
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log(`Server is running`);
+let port = process.env.PORT;
+if (port == null || port == "") {
+	port = 3000;
+}
+
+app.listen(port, () => {
+	console.log("Server started successfully.");
 });
